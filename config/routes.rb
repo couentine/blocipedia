7442506a9f54e2fa69d4fcfd3b47e071f7cb4ed4
Quentin Guillaume <quent.guillaume@gmail.com>
2017-08-08 20:41:58 +0000

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :wikis
- resources :charges, only: [:new, :create, :downgrade] do
+ resources :charges, only: [:new, :create] do
    post :downgrade
  end
   get 'welcome/index'
