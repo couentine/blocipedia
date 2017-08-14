4a972e68ab207d5fe347d3cad6943f7a961b56a3
Quentin Guillaume <quent.guillaume@gmail.com>
2017-08-14 22:53:27 +0000

    recarpet

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,3 +1,5 @@
+include WikisHelper
+
 class WikisController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
   
--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,4 +2,9 @@ module WikisHelper
     def user_is_authorized_for_wikis?
     current_user
 end
+
+ def markdown(text)
+    extensions = [:tables, :fenced_code_blocks, :autolink, :strikethrough, :underline, :highlight, :quote, :footnotes]
+Markdown.new(text, extensions).to_html.html_safe
+end
 end
